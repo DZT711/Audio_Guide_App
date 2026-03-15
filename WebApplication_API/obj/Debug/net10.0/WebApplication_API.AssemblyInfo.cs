5a4475d45cd0ab72cd72cff406a04bb234ca9ff9
DZT711 <nguyensihuynsh711@gmail.com>
2026-03-15 23:55:08 +0700

    note

--- WebApplication_API/obj/Debug/net10.0/WebApplication_API.AssemblyInfo.cs
+++ WebApplication_API/obj/Debug/net10.0/WebApplication_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05e4e493e396e6a4d1c55ec0ad920ebfb246caf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f9e808faea37a01251dfc4110bb3667c433834")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
