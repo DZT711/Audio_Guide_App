cf1b47001645cf779bbd5098cf26bde4ac46cb18
DZT711 <nguyensihuynsh711@gmail.com>
2026-03-19 10:50:13 +0700

    checkout

--- MauiApp_Mobile/obj/Debug/net10.0-windows10.0.19041.0/win-x64/MauiApp_Mobile.AssemblyInfo.cs
+++ MauiApp_Mobile/obj/Debug/net10.0-windows10.0.19041.0/win-x64/MauiApp_Mobile.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp_Mobile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c05e4e493e396e6a4d1c55ec0ad920ebfb246caf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5a4475d45cd0ab72cd72cff406a04bb234ca9ff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp_Mobile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp_Mobile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
