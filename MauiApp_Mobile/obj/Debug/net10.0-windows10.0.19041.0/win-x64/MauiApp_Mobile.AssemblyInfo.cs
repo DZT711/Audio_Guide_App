2372e43f6c5c9fea8b737a1893f74719f6e066c8
DZT711 <nguyensihuynsh711@gmail.com>
2026-03-24 10:51:36 +0700

    files tree

--- MauiApp_Mobile/obj/Debug/net10.0-windows10.0.19041.0/win-x64/MauiApp_Mobile.AssemblyInfo.cs
+++ MauiApp_Mobile/obj/Debug/net10.0-windows10.0.19041.0/win-x64/MauiApp_Mobile.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp_Mobile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7b183a9522e2f49f3b30f651a2e420a5abf59fcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a0af4cefc20586b55791a1fa3c661bc45b9a4ecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp_Mobile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp_Mobile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApplication_API/obj/Debug/net10.0/WebApplication_API.AssemblyInfo.cs
+++ WebApplication_API/obj/Debug/net10.0/WebApplication_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b183a9522e2f49f3b30f651a2e420a5abf59fcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0af4cefc20586b55791a1fa3c661bc45b9a4ecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
